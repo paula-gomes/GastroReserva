5118e41a9fdc4040cdbfa0bd341e06f54d47ea0d
EliPeixoto <eliane.peixoto@outlook.com>
2024-09-12 08:24:38 -0300

    Teste finalizado: Domain.Restaurante

--- src/test/java/com/grupo19/GastroReserva/domain/entities/restaurante/RestauranteTest.java
+++ src/test/java/com/grupo19/GastroReserva/domain/entities/restaurante/RestauranteTest.java
@@ -20,7 +20,6 @@ public class RestauranteTest {
         restaurante.setCadeirasDisponiveis(50);
         restaurante.ocuparCadeiras(10);
 
-
         Assertions.assertNotNull(restaurante.getNome(), "Nome não deve ser nulo");
         Assertions.assertNotNull(restaurante.getEndereco(), "Endereço não deve ser nulo");
         Assertions.assertNotNull(restaurante.getTipoDeCozinha(), "Tipo de Cozinha não deve ser nulo");
@@ -28,6 +27,13 @@ public class RestauranteTest {
         Assertions.assertNotNull(restaurante.getCapacidade(), "Capacidade não deve ser nula");
         Assertions.assertNotNull(restaurante.getCadeirasDisponiveis(), "Cadeiras Disponíveis não devem ser nulas");
         Assertions.assertEquals(40, restaurante.getCadeirasDisponiveis(), "Cadeiras Disponíveis deve ser 40 após ocupar 10 cadeiras");
+    }
+
+    @Test
+    public void numeroDeCadeirasOcupadasEValido() {
+        Restaurante restaurante = new Restaurante("Restaurante Exemplo", new Endereco(), "Italiana", new HorarioDeFuncionamento(LocalTime.of(8,0), LocalTime.of(22,0)), 100);
+        restaurante.ocuparCadeiras(10);
+        Assertions.assertEquals(90, restaurante.getCadeirasDisponiveis(), "Cadeiras Disponiveis deve ser 90 apos ocupar 10 cadeiras");
     }
 
 }
