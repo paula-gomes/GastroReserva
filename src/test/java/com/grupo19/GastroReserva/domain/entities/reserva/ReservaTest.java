6cffeab5aecfa58d7db1ad8f43f0a5ea6016f5f1
EliPeixoto <eliane.peixoto@outlook.com>
2024-09-14 12:47:53 -0300

    Finalizado Teste: domain.entities.reserva.Reserva

---
+++ src/test/java/com/grupo19/GastroReserva/domain/entities/reserva/ReservaTest.java
@@ -0,0 +1,36 @@
+package com.grupo19.GastroReserva.domain.entities.reserva;
+
+import com.grupo19.GastroReserva.domain.Endereco;
+import com.grupo19.GastroReserva.domain.HorarioDeFuncionamento;
+import com.grupo19.GastroReserva.domain.entities.cliente.Cliente;
+import com.grupo19.GastroReserva.domain.entities.restaurante.Restaurante;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ReservaTest {
+
+    @Test
+    public void deveRealizarReserva() {
+
+        Cliente cliente = new Cliente("123456789120", "nomeCliente", "email@teste.com");
+        Endereco endereco = new Endereco("12345-123", "logradouro", "12", "bairro", "cidade", "SP");
+        HorarioDeFuncionamento horarioDeFuncionamento = new HorarioDeFuncionamento(LocalTime.of(9, 0), LocalTime.of(18, 0));
+        LocalTime horario = LocalTime.of(17, 30);
+        Restaurante restaurante = new Restaurante("Restaurante Teste", endereco, "Italiana", horarioDeFuncionamento, 50, 100);
+        LocalDate data = LocalDate.of(2024, 9, 14);
+        Reserva reserva = new Reserva(cliente, restaurante, horario, data, 4);
+
+        assertNotNull(reserva);
+        assertEquals(cliente, reserva.getCliente());
+        assertEquals(restaurante, reserva.getRestaurante());
+        assertEquals(horario, reserva.getHorario());
+        assertEquals(data, reserva.getData());
+        assertEquals(4, reserva.getQuantidade());
+
+    }
+}
