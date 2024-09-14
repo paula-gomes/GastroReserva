bbabd1fe89d678b6362c36de99562d4e26a421c1
EliPeixoto <eliane.peixoto@outlook.com>
2024-09-14 19:52:33 -0300

    Teste application

---
+++ src/test/java/com/grupo19/GastroReserva/application/gateways/cliente/ExcluirClienteInterfaceTest.java
@@ -0,0 +1,41 @@
+package com.grupo19.GastroReserva.application.gateways.cliente;
+
+import com.grupo19.GastroReserva.domain.entities.cliente.Cliente;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+public class ExcluirClienteInterfaceTest {
+        @Mock
+        private ExcluirClienteInterface excluirClienteInterface;
+        private AutoCloseable openMocks;
+
+        @BeforeEach
+        void configurar() {
+            openMocks = MockitoAnnotations.openMocks(this);
+        }
+
+        @AfterEach
+        void derrubar() throws Exception {
+            if (openMocks != null) {
+                openMocks.close();
+            }
+        }
+
+        @Test
+        public void deveChamarExcluirCliente() {
+
+            Cliente cliente = new Cliente();
+            cliente.setNome("NomeCliente");
+            cliente.setEmail("cliente@email.com");
+
+            excluirClienteInterface.excluirCliente(cliente);
+
+            verify(excluirClienteInterface).excluirCliente(cliente);
+        }
+}
+
---
+++ src/test/java/com/grupo19/GastroReserva/application/gateways/cliente/ListarClienteInterfaceTest.java
@@ -0,0 +1,55 @@
+package com.grupo19.GastroReserva.application.gateways.cliente;
+
+import com.grupo19.GastroReserva.domain.entities.cliente.Cliente;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class ListarClienteInterfaceTest {
+    @Mock
+    private ListarClientesInterface listarClientesInterface;
+
+    private AutoCloseable openMocks;
+
+    @BeforeEach
+    void configurar() {
+        openMocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void derrubar() throws Exception {
+        if (openMocks != null) {
+            openMocks.close();
+        }
+    }
+
+    @Test
+    public void deveListarClientesComSucesso() {
+        List<Cliente> clientes = new ArrayList<>();
+        Cliente cliente1 = new Cliente();
+        cliente1.setNome("ClienteNome1");
+        cliente1.setEmail("cliente1@email.com");
+        clientes.add(cliente1);
+
+        Cliente cliente2 = new Cliente();
+        cliente2.setNome("ClienteNome2");
+        cliente2.setEmail("cliente2@email.com");
+        clientes.add(cliente2);
+
+        when(listarClientesInterface.listarClientes()).thenReturn(clientes);
+
+        List<Cliente> clientesListados = listarClientesInterface.listarClientes();
+
+        assertEquals(2, clientesListados.size());
+        assertEquals("ClienteNome1", clientesListados.get(0).getNome());
+        assertEquals("ClienteNome2", clientesListados.get(1).getNome());
+    }
+}
