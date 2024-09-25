d51beea2fb3072f14f59a04484d3c0d2d6f6e41a
EliPeixoto <eliane.peixoto@outlook.com>
2024-09-25 08:28:31 -0300

    update

--- src/main/java/com/grupo19/gastroreserva/infra/persistence/restaurante/RestauranteRepository.java
+++ src/main/java/com/grupo19/gastroreserva/infra/persistence/restaurante/RestauranteRepository.java
@@ -4,13 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
-public interface RestauranteRepository {
-    RestauranteEntity save(RestauranteEntity entity);
-    Optional<RestauranteEntity> findById(Long id);
-    List<RestauranteEntity> findAll();
-    void delete(RestauranteEntity entity);
-=======
 public interface RestauranteRepository extends JpaRepository<RestauranteEntity, Long> {
->>>>>>> 004733093263bbc291e9cab8410a02c3a5238b5e
 }
